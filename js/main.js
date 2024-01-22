9806e9fe324a6ee1927a11a306df867145d083e1
vbasoon <vbasoon@gmail.com>
2024-01-22 11:10:17 +0200

    reviews

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+      slidesPerView: 2,
+      spaceBetween: 22,
+  //direction: 'vertical',
+  loop: true,
+  // slidesPerView: 1,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.review-next',
+    prevEl: '.review-prev',
+  },
+
+  
+});
