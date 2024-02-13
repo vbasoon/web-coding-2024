be3cc2ab634c12ae55e4f3414112e455efa9ee2c
vbasoon <vbasoon@gmail.com>
2024-02-13 14:31:32 +0200

    footer team

--- js/main.js
+++ js/main.js
@@ -21,3 +21,30 @@ const menuList = document.querySelector('.menu__list');
 menuBtn.addEventListener('click', () => {
   menuList.classList.toggle('menu--open');
 })
+
+const swiper2 = new Swiper('.team-slider', {
+  // slidesPerView: 2,
+  // spaceBetween: 20,
+  // loop: true,
+  
+  // navigation: {
+  //   nextEl: '.swiper-button-next',
+  //   prevEl: '.swiper-button-prev',
+  // },
+});
+
+// Додати цю частину коду
+
+if (window.innerWidth <= 768) {
+  swiper2.init();
+} else {
+  swiper2.destroy();
+}
+
+window.addEventListener('resize', () => {
+  if (window.innerWidth <= 768) {
+    swiper2.init();
+  } else {
+    swiper2.destroy();
+  }
+});
